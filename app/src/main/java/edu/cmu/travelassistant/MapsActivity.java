c5beb52e00dc49bf7a1d9f45f0779d0a36d1c4d8
poojith <poojithjain@live.com>
2017-05-03 02:47:13 -0400

    Initial commit.

---
+++ app/src/main/java/edu/cmu/travelassistant/MapsActivity.java
@@ -0,0 +1,44 @@
+package edu.cmu.travelassistant;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng pittsburgh = new LatLng(40.4435, -79.9435);
+        mMap.addMarker(new MarkerOptions().position(pittsburgh).title("Pittsburgh, PA"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(pittsburgh));
+        mMap.setMinZoomPreference(15.0f);
+        mMap.setMaxZoomPreference(21.0f);
+    }
+}
