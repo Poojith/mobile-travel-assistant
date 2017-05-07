47c3422fc8d8cce7cb7f2cec2586fc65de4da6ee
Anshit Agarwal <a.anshit@gmail.com>
2017-05-07 01:54:36 -0400

    Testing gitignore

--- app/src/main/java/edu/cmu/travelassistant/MapsActivity.java
+++ app/src/main/java/edu/cmu/travelassistant/MapsActivity.java
@@ -17,7 +17,6 @@ import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
