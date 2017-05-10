9f333cd35503e87be39c7d8272eeb0d7928419e9
Anshit Agarwal <a.anshit@gmail.com>
2017-05-09 21:19:45 -0400

    Correcting bus route display

--- app/src/main/java/edu/cmu/travelassistant/util/RealTimeAPITask.java
+++ app/src/main/java/edu/cmu/travelassistant/util/RealTimeAPITask.java
@@ -37,7 +37,7 @@ import java.util.Set;
 
 public class RealTimeAPITask extends AsyncTask {
     String queryResult;
-    String baseUrl = "http://truetime.portauthority.org/bustime/api/v3/getstops?key=Gg5eAVrmgNc3U5kC5PcFfcQGz&format=json&rtpidatafeed=Port%20Authority%20Bus&dir=INBOUND&rt=";
+    String baseUrl = "http://truetime.portauthority.org/bustime/api/v3/getstops?key=QzLiAG6tuHzPqqii3ETFuwTsZ&format=json&rtpidatafeed=Port%20Authority%20Bus&dir=INBOUND&rt=";
 
     GoogleMap mMap;
     private Context context;
@@ -102,8 +102,7 @@ public class RealTimeAPITask extends AsyncTask {
                 stops.add(stop);
             }
         } catch (JSONException e) {
-            //e.printStackTrace();
-            ;
+            e.printStackTrace();
         }
 
         return stops;
